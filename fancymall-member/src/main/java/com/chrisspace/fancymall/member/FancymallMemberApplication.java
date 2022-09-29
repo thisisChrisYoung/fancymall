b34bfce972c631293b3f134143c7e9883c4b30fa
thisisChrisYoung <136710225@qq.com>
2022-09-29 23:54:56 +0800

    引入nacos

--- fancymall-member/src/main/java/com/chrisspace/fancymall/member/FancymallMemberApplication.java
+++ fancymall-member/src/main/java/com/chrisspace/fancymall/member/FancymallMemberApplication.java
@@ -2,8 +2,9 @@ package com.chrisspace.fancymall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FancymallMemberApplication {
 
